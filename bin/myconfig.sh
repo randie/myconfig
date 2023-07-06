e1a3fa40e7839c76def6d6cf5ea993873f798863
Randie <randie.bemis@gmail.com>
2023-07-06 00:09:06 -0700

    clean up bin/myconfig.sh

--- bin/myconfig.sh
+++ bin/myconfig.sh
@@ -2,9 +2,13 @@
 
 NOW=$(date +%y%m%d%H%M) 
 MYCONFIG=myconfig
-GITHUB_REPO="git@github.com:randie/$MYCONFIG.git"
 BARE_REPO="$HOME/${MYCONFIG}-bare"
+GITHUB_REPO="git@github.com:randie/$MYCONFIG.git"
 
+
+#
+# install homebrew if not already installed
+#
 install_homebrew() {
     if [[ ! -x /usr/local/Homebrew/bin/brew ]]
     then
@@ -22,6 +26,8 @@ install_homebrew() {
 # install packages and apps with homebrew
 #
 brew_install_packages() {
+    install_homebrew
+
     b=$MYCONFIG/.Brewfile
     if [[ ! -e $b ]]
     then
@@ -39,9 +45,18 @@ brew_install_packages() {
 
 
 #
+# install oh_my_zsh if not already installed
+#
+install_oh_my_zsh() {
+    echo "WARNING! install_oh_my_zsh() is not implemented yet"
+}
+
+
+#
 # create a bare github repo to track config files
 #
 create_bare_repo() {
+    # TODO: check that clone does not already exist
     git clone --bare $GITHUB_REPO $BARE_REPO
 }
 
@@ -73,30 +88,36 @@ backup_existing_config() {(
 )}
 
 
+apply_my_config() {(
+    # NOTE: The parens surrounding this function is to confine its cd's locally
+
+    cd $HOME
+    git --git-dir=$BARE_REPO --work-tree=$HOME config --local status.showUntrackedFiles no
+    git --git-dir=$BARE_REPO --work-tree=$HOME checkout
+)}
+
+
+wrap_up() {
+    ALIAS_C="alias c=\"git --no-pager --git-dir=$BARE_REPO --work-tree=$HOME\""
+    echo
+    echo "We're done!"
+    echo
+    echo Just make sure the following alias is in your .zshrc file:
+    echo $ALIAS_C
+    echo
+    echo Now start up a new shell, and you should be good to go.
+    echo
+}
+
 #=======================#
 #                       #
 #    M A I N L I N E    #
 #                       #
 #=======================#
 
-#brew_install_packages
-#install_oh_my_zsh
+brew_install_packages
+install_oh_my_zsh
 create_bare_repo
 backup_existing_config
-
-# apply your configuration
-cd $HOME
-git --git-dir=$BARE_REPO --work-tree=$HOME config --local status.showUntrackedFiles no
-git --git-dir=$BARE_REPO --work-tree=$HOME checkout
-
-
-# wrap up
-ALIAS_C="alias c=\"git --no-pager --git-dir=$BARE_REPO --work-tree=$HOME\""
-echo
-echo "We're done!"
-echo
-echo Just make sure the following alias is in your .zshrc file:
-echo $ALIAS_C
-echo
-echo Now start up a new shell, and you should be good to go.
-echo
+apply_my_config
+wrap_up
